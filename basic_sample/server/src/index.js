a4f6106fac80838eba6ad817c0723cda28f8b9d3
Unknown <steven.chou@uniqestate.com>
2018-04-08 15:51:42 +0800

    Server Store Creation

--- basic_sample/server/src/index.js
+++ basic_sample/server/src/index.js
@@ -8,13 +8,20 @@
 // const Home = require('./client/components/Home').default;
 
 import express from 'express';
+
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 const app = express();
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-  res.send(renderer(req));
+  const store = createStore();
+
+  // Some logic to initialize
+  // and load data into store
+
+  res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
