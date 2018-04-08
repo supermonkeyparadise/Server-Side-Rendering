1e1cf545e59f5045d2dd02ffc45d0fb2b07337e3
Unknown <steven.chou@uniqestate.com>
2018-04-08 15:30:41 +0800

    Browser Store Creation

--- basic_sample/server/src/client/client.js
+++ basic_sample/server/src/client/client.js
@@ -2,13 +2,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import Routes from './Routes';
 
+const store = createStore(reducers, {}, applyMiddleware('thunk'));
+
 const routerJSX = (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  // 當 redux store change，Provider 會通知有 connect 的 component，讓它們重新 render
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>
 );
 
 // 在執行 bundle.js 前， server 已經 rendering data to browser 了
