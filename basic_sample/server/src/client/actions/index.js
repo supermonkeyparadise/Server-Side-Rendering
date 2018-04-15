1786991471ba829f3e0263e0500af0a9cbae5262
Unknown <steven.chou@uniqestate.com>
2018-04-15 15:03:14 +0800

    xss attack

--- basic_sample/server/src/client/actions/index.js
+++ basic_sample/server/src/client/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_USERS = 'fetch_users';
 export const fetchUsers = () => async dispatch => {
+  // https://react-ssr-api.herokuapp.com/users/xss 
   const res = await axios.get('https://react-ssr-api.herokuapp.com/users');
 
   dispatch({
