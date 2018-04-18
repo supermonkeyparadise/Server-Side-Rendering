2f82708e46fc214413bdf5c3a357c8ea40f5e848
Unknown <steven.chou@uniqestate.com>
2018-04-18 16:43:35 +0800

    Handling Redirects

--- basic_sample/server/src/index.js
+++ basic_sample/server/src/index.js
@@ -65,6 +65,11 @@ app.get('*', (req, res) => {
     const context = {};
     const content = renderer(req, store, context);
 
+    console.log('## context:', context);
+    if (context.url) {
+      console.log('## gg')
+      return res.redirect(301, context.url);
+    }
     if (context.notFound) {
       res.status(404);
     }
